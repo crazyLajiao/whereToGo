7b05dfb52d9347342cd1ed22afbc76b4ca27803e
niuniu <1194802511@qq.com>
2018-05-26 19:46:13 +0800

    'addHomeHeader'

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//移动端300ms延迟问题
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+//1像素边框问题
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<div class="iconfont back-icon">&#xe624;</div>
+		</div>
+		<div class="header-input">
+			<span class="iconfont">&#xe632;</span>
+			输入城市、游玩、主题
+		</div>
+		<div class="header-right">
+			合肥
+			<span class="iconfont arrow-icon">&#xe64a;</span>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.header
+		display: flex
+		line-height: $headerHeight
+		background: $bgColor
+		color: #fff
+		.header-left
+			width: .64rem
+			float: left
+			margin-top: -.1rem
+			.back-icon
+				text-align: center
+				margin-top: .22rem
+				font-size: .4rem
+		.header-input
+			flex: 1
+			height: .64rem
+			line-height: .64rem
+			margin-top: .19rem
+			margin-left: .2rem
+			padding-left: .2rem
+			background: #fff
+			color: #ccc
+			border-radius: .1rem
+		.header-right
+			min-width: 1.04rem
+			padding: 0 .1rem
+			float: right
+			text-align: center
+			margin-top: .12rem
+			color: #fff
+			.arrow-icon
+				margin-left: -.04rem
+				font-size: .2rem
+</style>
